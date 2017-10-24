0ca585fa4b67eb521d3abb4a2b514eac00205616
Jacobra93 <Jacobra93@gmail.com>
2017-10-23 20:29:33 -0500

    Initial project commit with Game class and Character class

---
+++ src/Character.java
@@ -0,0 +1,16 @@
+public class Character {
+    public String name;
+    public double level;
+    public double magicLevel;
+    public double stealthLevel;
+    public double strengthLevel;
+
+    public Character (String name) {
+        this.name = name;
+        this.level = 1;
+        this.magicLevel = 1;
+        this.stealthLevel = 1;
+        this.strengthLevel = 1;
+    }
+
+}
